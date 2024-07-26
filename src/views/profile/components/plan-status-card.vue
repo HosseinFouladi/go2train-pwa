ecfe31167921774ea386334b6c6e70c31337eeca
emad <emadbaqeri@outlook.com>
2024-07-26 17:38:19 +0330

    bugfix(ui): hide plan-status card for first sign-ups

--- src/views/profile/components/plan-status-card.vue
+++ src/views/profile/components/plan-status-card.vue
@@ -7,8 +7,8 @@ import { DateInfo, PlanStatusBadge } from '.'
 import { RefreshIcon } from '@/components/icons'
 import { useMutation } from '@tanstack/vue-query'
 import { InlineInfo, LineDivider, Button } from '@/components'
-import { purchaseByCallback, type PurchasePlanByCallbackParams } from '@/queries'
 import { useGetCurrentPlan } from '@/queries/profile/current-plan-query'
+import { purchaseByCallback, type PurchasePlanByCallbackParams } from '@/queries'
 
 const { data } = useGetCurrentPlan()
 const currentPlan = computed(() => data.value?.data.results)
@@ -22,7 +22,7 @@ const daysLeft = computed(
 const { mutate: purchasePlan } = useMutation({
   mutationKey: ['plans'],
   mutationFn: (params: PurchasePlanByCallbackParams) => purchaseByCallback(params),
-  onSuccess({ data }, _variables, _context) {
+  onSuccess({ data }, _v, _c) {
     if (!window) return
     window.open(data.results.url, '_blank')
   }
@@ -31,12 +31,10 @@ const { mutate: purchasePlan } = useMutation({
 
 <template>
   <section
+    v-if="currentPlan?.priority === 0 && currentPlan?.prev_plan !== null"
     class="w-full p-6 space-y-4 paper md:card rounded-2xl bg-neutral-white h-fit"
   >
-    <div
-      v-if="currentPlan?.priority === 0 && currentPlan?.prev_plan !== null"
-      class="w-full space-y-4"
-    >
+    <div class="w-full space-y-4">
       <h2 class="text-center text-h6 font-demi-bold text-danger-500">
         اشتراک شما به پایان رسیده!
       </h2>
