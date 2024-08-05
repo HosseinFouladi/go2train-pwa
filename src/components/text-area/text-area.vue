9c9cc8f0eccc16ff51b732b529a6096eefc4fcd2
emad <emadbaqeri@outlook.com>
2024-08-05 11:08:46 +0330

    fix(ui): fix text-area diabled pointer events

--- src/components/text-area/text-area.vue
+++ src/components/text-area/text-area.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, defineEmits } from 'vue'
 
-import type { TextAreaProps } from './textArea'
+import type { TextAreaProps } from './text-area'
 
 import { cn } from '@/utils'
 
@@ -16,7 +16,7 @@ const props = withDefaults(defineProps<TextAreaProps>(), {
 })
 const emit = defineEmits(['modelValue'])
 
-const text = ref(props.value||'')
+const text = ref(props.value || '')
 
 const getTextAreaValue = () => {
   emit('modelValue', text.value)
@@ -30,11 +30,13 @@ const getTextAreaValue = () => {
       :placeholder="placeHolder"
       :rows="rows"
       v-model="text"
-      class="p-4 overflow-hidden border rounded-2xl border-secondary-500 text-st-two"
+      class="p-4 overflow-hidden border text-secondary-600 focus:outline-none rounded-2xl border-secondary-500 text-st-two"
       :class="
         cn({
           'w-full': fluid,
-          'w-fit': !fluid
+          'w-fit': !fluid,
+          'hover:text-secondary-700 focus:text-secondary-900': !props.disabled,
+          'pointer-events-none': props.disabled
         })
       "
       v-bind="$attrs"
