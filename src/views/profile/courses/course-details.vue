12128db6c94ce45a476d59aceeac2603161d2823
emad <emadbaqeri@outlook.com>
2024-08-12 17:38:26 +0330

    fix(core): fix improper imports

--- src/views/profile/courses/course-details.vue
+++ src/views/profile/courses/course-details.vue
@@ -1,16 +1,12 @@
-<template>
-    <div class="w-full">
-        <Header/>
-        <div class="relative z-50 p-4 -mt-4 bg-neutral-white rounded-t-2xl">
-            <IntroductionVideo/>
-        </div>
-    </div>
-</template>
-
 <script setup lang="ts">
-    import { Header,IntroductionVideo } from '../components';
+import { Header, IntroductionVideo } from './components'
 </script>
 
-<style scoped>
-
-</style>
+<template>
+  <div class="w-full">
+    <Header />
+    <div class="relative z-50 p-4 -mt-4 bg-neutral-white rounded-t-2xl">
+      <IntroductionVideo />
+    </div>
+  </div>
+</template>
