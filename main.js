3e777a39668071258c52c2b014d130864dbd3a2d
tina.hosseinpour <tina.hosseinpour@g2holding.org>
2024-09-03 16:36:38 +0330

    feat/export electronjs

--- main.js
+++ main.js
@@ -5,21 +5,24 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, '/src/assets/icons/favicon.icns'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
   win.loadFile("dist/index.html");
-  // if (process.env.NODE_ENV !== 'production') {
-  //   win.loadURL('http://localhost:3000');
-  //   win.webContents.openDevTools();
-  // } else {
-  //   win.loadFile(path.join(__dirname, 'dist', 'index.html'));
-  // }
+  if (process.env.NODE_ENV !== 'production') {
+    win.loadURL('http://localhost:5173');
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, 'dist', 'index.html'));
+  }
 }
 
 app.whenReady().then(() => {
